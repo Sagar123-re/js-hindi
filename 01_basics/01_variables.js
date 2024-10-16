cce54df560d1456c2d44a0e24e7a82b09380cc5b
Sagar123-re <sagarnautiyal111@gmail.com>
2024-10-16 16:36:14 +0000

    Datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const account_Id=144553
+let accountEmail="sagarnautiyal111@gmail.com"
+var accountpassword="12345"
+accountCity="Jaipur"
+let accountstate
+accountEmail="sagarmail.com"
+accountpassword="123"
+//account_Id=2
+//prefer not to use var because of scope issue in block and functional
+console.log(account_Id)
+console.table([account_Id,accountEmail,accountpassword,accountstate])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,7 @@
+let name="sagar"
+let age=3
+let isloggedIn=false
+let state="null"
+console.table([name,age,state])
+console.log(typeof isloggedIn)
+
